d49ec95fb8d6db236656104d6373b8776f513f05
mike-ki17 <michael16cd@gmail.com>
2025-08-30 10:58:55 -0500

    fix: conexion base de datos

--- config/db.php
+++ config/db.php
@@ -1,12 +1,27 @@
 <?php
-$host = "localhost";
-$user = "root";   // tu usuario MySQL
-$pass = "";       // tu contraseña
-$db   = "sistema_facturacion";
+require_once __DIR__ . '/../vendor/autoload.php'; // ruta correcta
 
-$conn = new mysqli($host, $user, $pass, $db);
+use Dotenv\Dotenv;
+
+// Cargar el .env desde la raíz
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Probar si se cargó
+// var_dump(getenv('DB_HOST'));
+
+// Conexión MySQL
+$host = $_ENV['DB_HOST'] ?? null;
+$user = $_ENV['DB_USER'] ?? null;
+$pass = $_ENV['DB_PASS'] ?? null;
+$db   = $_ENV['DB_NAME'] ?? null;
+$port = (int) ($_ENV['DB_PORT'] ?? 3306);
+
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
+
 ?>
